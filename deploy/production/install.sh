f1c904c553a88d79f3af2b9fde28750e70f04a2a
smskin <serg@msoinvest.com>
2022-04-30 14:50:46 +0300

    fixed installation script

--- deploy/production/install.sh
+++ deploy/production/install.sh
@@ -9,7 +9,7 @@ docker network create webproxy
 docker compose -f docker-compose.yml -f docker-compose.production.yml build app
 docker compose -f docker-compose.yml -f docker-compose.production.yml build
 docker compose -f docker-compose.yml -f docker-compose.production.yml up -d php-cli
-docker-compose -f docker-compose.yml -f docker-compose.production.yml exec php-cli php artisan storage:link
+docker compose -f docker-compose.yml -f docker-compose.production.yml exec php-cli php artisan storage:link
 docker compose -f docker-compose.yml -f docker-compose.production.yml --profile install up minio-install
 docker compose -f docker-compose.yml -f docker-compose.production.yml --profile install down
 docker compose -f docker-compose.yml -f docker-compose.production.yml down
