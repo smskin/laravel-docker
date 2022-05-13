60a8ad3243db7194137c43e4e70b0c7b9c624cc7
smskin <serg@msoinvest.com>
2022-05-13 23:06:33 +0300

    fixed exception

--- app/Modules/Core/Exceptions/ConflictException.php
+++ app/Modules/Core/Exceptions/ConflictException.php
@@ -3,8 +3,23 @@
 namespace App\Modules\Core\Exceptions;
 
 use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Contracts\Support\Responsable;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
-class ConflictException extends Exception
+class ConflictException extends Exception implements Responsable
 {
+    public function __construct(string $message = "", int $code = 409, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
+    public function toResponse($request): \Illuminate\Http\Response|Response|Application|ResponseFactory
+    {
+        return response([
+            'message' => $this->message
+        ], $this->code);
+    }
 }
